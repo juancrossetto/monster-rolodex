e309f76bcb97144d3c6e951a0d0bcbf4430cc115
Juanma Crossetto <juancrossetto@gmail.com>
2020-08-12 11:02:41 -0300

    Updates

---
+++ precache-manifest.bb2c302146b8094e91f41025965ad4a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44c6e3501053efbf122dd5ba469f25f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "99dd4d76cb1ccb2a2976",
+    "url": "/monster-rolodex/static/css/main.b1bdce88.chunk.css"
+  },
+  {
+    "revision": "2bd537cf25ce2a0bc84e",
+    "url": "/monster-rolodex/static/js/2.0376a406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.0376a406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99dd4d76cb1ccb2a2976",
+    "url": "/monster-rolodex/static/js/main.5fd6afd4.chunk.js"
+  },
+  {
+    "revision": "26e2e9c67742c3f19dca",
+    "url": "/monster-rolodex/static/js/runtime-main.892f43a5.js"
+  }
+]);
